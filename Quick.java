2f25b8da5356ffd8a9975042252f4e4c5a4952bc
almathaler <athaler00@stuy.edu>
2019-03-12 22:45:54 -0400

    not sure if start in partition is supposed to be 0 or 1, rewrote so it's 0

--- Quick.java
+++ Quick.java
@@ -1,18 +1,69 @@
 import java.util.Random;
 import java.util.Arrays;
 public class Quick{
+  //notes: for partition, start should be 1 not 0, 0th index reserved for swapping w pivot
+  //keep this in mind when reading over quicksort
   public static void main(String[] args){
-    int[] data = {4, 4, 5, 1, 65, 2, 7, 8, 45, 3, 6, 754, 23, 1, -765, 89};
-    partition(data, 1, data.length-1);
-  }
+    //for (int i = 0; i<25; i++){
+    //  for (int i = 0; i<10; i++){
+    //int[] data = new int[25];
+    for (int i = 0; i<25; i++){
+      Random rng = new Random();
+      int size = rng.nextInt(500);
+      int[] data = new int[size];
+      for (int c = 0; c<size; c++){
+        data[c] = rng.nextInt(5000);
+      }
+      //System.out.println(Arrays.toString(data));
+      int pivotInd = partition(data, 0, data.length-1);
+      if (sortedIsh(data, pivotInd)){
+        System.out.println("correct");
+      }else{
+        System.out.println("NOT SORTED: " + Arrays.toString(data));
+      }
+    //  System.out.println("correctly partitioned?: " + sortedIsh(data, pivotInd));
+      //int k = rng.nextInt(size);
+      //System.out.println("\ndata: " + Arrays.toString(data));
+      //System.out.println("the " + k + "th smallest element: " + quickselect(data, k));
+      //insertionSort(data);
+      //System.out.println("the " + k + "th smallest element according to insertion: "+ data[k]);
+      //System.out.println(Arrays.toString(data));
+      //int pivotInd = partition(data, 1, data.length-1);
+      //System.out.println("correctly partitioned?: " + sortedIsh(data, pivotInd));
+    }
+  //System.out.println(partition(data, 1, data.length-1));
+  //  }
+  //}
+ }
   /*return the value that is the kth smallest value of the array.
  */
  //have to have this account for arrays of size 1
+ /*Modify the array to be in increasing order.
+ */
+ public static void quicksort(int[] data){
+   //recursively, do partition(data, 1, data.length - 1) and then partition of that
+   //partition call's return like this: partition(data, 1, return - 1) and also do other side:
+   //partition(data, return + 1, data.length-1)
+   int pivotInd = partition(data, 1, data.length - 1);
+   quicksortHelp(data, 1, pivotInd-1);
+   quicksortHelp(data, pivotInd+2, data.length-1); //doing this bc last space is supposed to be reserved to put in pivot, don't want current pivot affected
+   //keep wroking on this!
+ }
+ private static void quicksortHelp(int[] data, int start, int end){ //here start and end represent beginning of array to be processed and end
+   if (start != end){
+     int pivotInd = partition(data, start, end);
+     quicksortHelp(data, 0, pivotInd-1); //less than half, don't wanna touch pivot
+     quicksortHelp(data, pivotInd+1, data.length-1 ); //don't wanna affect pivot, point before start is swapped w pivot
+   }//do one last call                                        //remember if pivot is behind start, start must be 2 infront of pivot so next chosen
+  quicksortHelp(data, start, end);                              //pivot can be placed
+  //what's above should do the last sort of data having 2 members, choosing to swap or no                                                //pivot can be placed
+ }
  public static int quickselect(int[] data, int k){
+   System.out.println("\nIN NEW QUICKSELECT CALL Searching for " + k);
    if (data.length > 1){
      boolean foundK = false;
      while (!foundK){ //don't really need boolean, bc this loop is broken by an internal return
-       int ind = partition(data, 1, data.length-1); //run partition, it's return is the final index of the partition
+       int ind = partition(data, 0, data.length-1); //run partition, it's return is the final index of the partition
        //add to k, the index to return, however many duplicates there are of each number. For example
        //of [1, 0, 0, 2, 3, 4, 4], if you want to get the 2rd smalles number you have to return the 3rd index, bc
        //there are two 0s. only run this until the end of k, ones past k don't matter.
@@ -28,35 +79,38 @@ public class Quick{
      return data[0];
    }
  }
-//checks for duplicates, returns first index of the next value that's not a duplicate of the previous
- private static int nextNotDuplicate(int[] data, int maybeDuplicate){
-   if (maybeDuplicate > 0 && data[maybeDuplicate] == data[maybeDuplicate-1]){//if current is the same as the one before
-     return nextNotDuplicate(data, maybeDuplicate+1); //check the next one to see if it's still the same, if not
-   }else{
-     return maybeDuplicate; //return that index
-   }
- }
 
 
   //start = 1 and end = data.length-1
+  //returns final index of chosen pivot
   private static int partition(int[] data, int start, int end){
+    if (start == end){
+      return start; //don't touch the array
+    }
     Random rng = new Random();
     int pivotInd;
     int pivot;
-    if (data[start] > data[end] && data[start] < data[(data.length-1)/2] || data[start] < data[end] && data[start] > data[(data.length-1)/2]){
+    if (data[start] > data[end] && data[start] < data[(end+start)/2] || data[start] < data[end] && data[start] > data[(end+start)/2]){
       pivot = data[start];
       pivotInd = start;
-    }else if (data[end] > data[start] && data[end] < data[(data.length-1)/2] || data[end] < data[start] && data[end] > data[(data.length-1)/2]){
+    }else if (data[end] > data[start] && data[end] < data[(end+start)/2] || data[end] < data[start] && data[end] > data[(end+start)/2]){
       pivot = data[end];
       pivotInd = end;
     }else{
-      pivotInd = (data.length - 1)/2;
+      pivotInd = (end+start)/2; //refrain from using 0s and data.length - 1, bc in quicksort we do subsets not whole array
       pivot = data[pivotInd];
     }
-
-    System.out.println("PIVOT: " + pivot + "\nPIVOT INDEX: " + pivotInd);
+    //there might be a problem w this ^^. in an array of size 2, data[(data.length-1)/2] will just be data[0], which is Start
+    //making first and second if statements false. so will default to the last statement, which will make pivot start ajd the bit
+    //at the end
+    //actaully it's not a problem bc pivot moved to 0, start can = end skipping the while loop
+    //and end statemetns will be evaluated, which just compare start (1) and pivot (0)
+    //System.out.println("PIVOT: " + pivot + "\nPIVOT INDEX: " + pivotInd);
   //  System.out.println("Array before movements: " + Arrays.toString(data));
-    swap(data, 0, pivotInd);//move pivot to back
+ //this bariavle exists so partition called on sub array won't affect other parts
+    int pivotAtStartInd = start;
+    swap(data, pivotAtStartInd, pivotInd);//move pivot to back
+    start++;
     while (start != end){//when you still have left to compare
       int dealWDupes = rng.nextInt(2);
       if (data[start] > pivot || data[start] == pivot && dealWDupes == 0){//compare
@@ -69,17 +123,17 @@ public class Quick{
     //  System.out.println("Start: " + start + " End: " + end);
     }
     if (data[start] < pivot){ //you need to switch the start and pivot
-      swap(data, start, 0);
+      swap(data, start, pivotAtStartInd);
   //    System.out.println("Moved pivot to appropriate index: " + start + "\n" + Arrays.toString(data));
       //System.out.println("Final index of " + pivot + " is ");
-      System.out.println("data looks like: " + Arrays.toString(data));
+      //System.out.println("data looks like: " + Arrays.toString(data));
       return start;
     }else{
-      swap(data, start-1, 0);
+      swap(data, start-1, pivotAtStartInd);
     //  System.out.println("Moved pivot to appropriate index: " + (start-1) + "\n" + Arrays.toString(data));
     //  System.out.println("Final index of " + pivot + " is ");
     //  System.out.println
-      System.out.println("data looks like: " + Arrays.toString(data));
+      //System.out.println("data looks like: " + Arrays.toString(data));
       return start-1;
     }
   }
@@ -91,11 +145,13 @@ public class Quick{
   private static boolean sortedIsh(int[] data, int pivotInd){
     for (int i = 0; i<pivotInd; i++){
       if (data[i] > data[pivotInd]){
+        System.out.println("data[" + i + "]: " + data[i] + " is not less than or equal to " + data[pivotInd]);
         return false;
       }
     }
     for (int iA = pivotInd+1; iA<data.length; iA++){
       if (data[iA] < data[pivotInd]){
+        System.out.println("data[" + iA + "]: " + data[iA] + " is not greater than or equal to " + data[pivotInd]);
         return false;
       }
     }
